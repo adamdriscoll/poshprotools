a5e23ed5e547a7593728e76f9e528b8591fe4afc
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2017-10-16 20:25:30 -0500

    Update server-performance-dashboard.ps1

--- examples/universal-dashboard/server-performance-dashboard.ps1
+++ examples/universal-dashboard/server-performance-dashboard.ps1
@@ -1,10 +1,10 @@
-Start-Dashboard -Content { 
-    New-Dashboard -Title "Server Performance Dashboard" -Color '#FF050F7F' -Content { 
-        New-Row {
-            New-Column -Size 6 -Content {
-                New-Row {
-                    New-Column -Size 12 -Content {
-                        New-Table -Title "Server Information" -Headers @(" ", " ") -Endpoint {
+Start-UdDashboard -Content {
+    New-UdDashboard -Title "Server Performance Dashboard" -Color '#FF050F7F' -Content {
+        New-UdRow {
+            New-UdColumn -Size 6 -Content {
+                New-UdRow {
+                    New-UdColumn -Size 12 -Content {
+                        New-UdTable -Title "Server Information" -Headers @(" ", " ") -Endpoint {
                             @{
                                 'Computer Name' = $env:COMPUTERNAME
                                 'Operating System' = (Get-CimInstance -ClassName Win32_OperatingSystem).Caption
@@ -14,9 +14,9 @@ Start-Dashboard -Content {
                         }
                     }
                 }
-                New-Row {
-                    New-Column -Size 3 -Content { 
-                        New-Chart -Title "Memory by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
+                New-UdRow {
+                    New-UdColumn -Size 3 -Content {
+                        New-UdChart -Title "Memory by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
                             Get-Process | ForEach-Object { [PSCustomObject]@{ Name = $_.Name; WorkingSet = [Math]::Round($_.WorkingSet / 1MB, 2) }} |  Out-ChartData -DataProperty "WorkingSet" -LabelProperty Name
                         } -Options @{
                             legend = @{
@@ -24,27 +24,27 @@ Start-Dashboard -Content {
                             }
                         }
                     }
-                    New-Column -Size 3 -Content { 
-                        New-Chart -Title "CPU by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
-                            Get-Process | ForEach-Object { [PSCustomObject]@{ Name = $_.Name; CPU = $_.CPU } } |  Out-ChartData -DataProperty "CPU" -LabelProperty Name
+                    New-UdColumn -Size 3 -Content {
+                        New-UdChart -Title "CPU by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
+                            Get-Process | ForEach-Object { [PSCustomObject]@{ Name = $_.Name; CPU = $_.CPU } } |  Out-UDChartData -DataProperty "CPU" -LabelProperty Name
                         } -Options @{
                             legend = @{
                                 display = $false
                             }
                         }
                     }
-                    New-Column -Size 3 -Content { 
-                        New-Chart -Title "Handle Count by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
-                            Get-Process | Out-ChartData -DataProperty "HandleCount" -LabelProperty Name
+                    New-UdColumn -Size 3 -Content {
+                        New-UdChart -Title "Handle Count by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
+                            Get-Process | Out-UDChartData -DataProperty "HandleCount" -LabelProperty Name
                         } -Options @{
                             legend = @{
                                 display = $false
                             }
                         }
                     }
-                    New-Column -Size 3 -Content { 
-                        New-Chart -Title "Threads by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
-                            Get-Process | ForEach-Object { [PSCustomObject]@{ Name = $_.Name; Threads = $_.Threads.Count } } |  Out-ChartData -DataProperty "Threads" -LabelProperty Name
+                    New-UdColumn -Size 3 -Content {
+                        New-UdChart -Title "Threads by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {
+                            Get-Process | ForEach-Object { [PSCustomObject]@{ Name = $_.Name; Threads = $_.Threads.Count } } |  Out-UDChartData -DataProperty "Threads" -LabelProperty Name
                         } -Options @{
                             legend = @{
                                 display = $false
@@ -52,69 +52,69 @@ Start-Dashboard -Content {
                         }
                     }
                 }
-                New-Row {
-                    New-Column -Size 12 -Content {
-                        New-Chart -Title "Disk Space by Drive" -Type Bar -AutoRefresh -Endpoint {
-                            Get-CimInstance -ClassName Win32_LogicalDisk | ForEach-Object { 
-                                    [PSCustomObject]@{ DeviceId = $_.DeviceID; 
-                                                       Size = [Math]::Round($_.Size / 1GB, 2); 
-                                                       FreeSpace = [Math]::Round($_.FreeSpace / 1GB, 2); } } | Out-ChartData -LabelProperty "DeviceID" -Dataset @(
-                                New-ChartDataset -DataProperty "Size" -Label "Size" -BackgroundColor "#80962F23" -HoverBackgroundColor "#80962F23"
-                                New-ChartDataset -DataProperty "FreeSpace" -Label "Free Space" -BackgroundColor "#8014558C" -HoverBackgroundColor "#8014558C"
+                New-UdRow {
+                    New-UdColumn -Size 12 -Content {
+                        New-UdChart -Title "Disk Space by Drive" -Type Bar -AutoRefresh -Endpoint {
+                            Get-CimInstance -ClassName Win32_LogicalDisk | ForEach-Object {
+                                    [PSCustomObject]@{ DeviceId = $_.DeviceID;
+                                                       Size = [Math]::Round($_.Size / 1GB, 2);
+                                                       FreeSpace = [Math]::Round($_.FreeSpace / 1GB, 2); } } | Out-UDChartData -LabelProperty "DeviceID" -Dataset @(
+                                New-UdChartDataset -DataProperty "Size" -Label "Size" -BackgroundColor "#80962F23" -HoverBackgroundColor "#80962F23"
+                                New-UdChartDataset -DataProperty "FreeSpace" -Label "Free Space" -BackgroundColor "#8014558C" -HoverBackgroundColor "#8014558C"
                             )
                         }
                     }
                 }
             }
-            New-Column -Size 6 -Content {
-                New-Row {
-                    New-Column -Size 6 -Content { 
-                        New-Monitor -Title "CPU (% processor time)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#80FF6B63' -ChartBorderColor '#FFFF6B63'  -Endpoint {
+            New-UdColumn -Size 6 -Content {
+                New-UdRow {
+                    New-UdColumn -Size 6 -Content {
+                        New-UdMonitor -Title "CPU (% processor time)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#80FF6B63' -ChartBorderColor '#FFFF6B63'  -Endpoint {
 						    try {
-								Get-Counter '\Processor(_Total)\% Processor Time' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-MonitorData
+								Get-Counter '\Processor(_Total)\% Processor Time' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
 							}
                             catch {
-								0 | Out-MonitorData
+								0 | Out-UDMonitorData
 							}
-                        } 
+                        }
                     }
-                    New-Column -Size 6 -Content { 
-                        New-Monitor -Title "Memory (% in use)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#8028E842' -ChartBorderColor '#FF28E842'  -Endpoint {
+                    New-UdColumn -Size 6 -Content {
+                        New-UdMonitor -Title "Memory (% in use)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#8028E842' -ChartBorderColor '#FF28E842'  -Endpoint {
 							try {
-								Get-Counter '\memory\% committed bytes in use' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-MonitorData
+								Get-Counter '\memory\% committed bytes in use' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
 							}
                             catch {
-								0 | Out-MonitorData
+								0 | Out-UDMonitorData
 							}
-                        } 
+                        }
                     }
                 }
-                New-Row {
-                    New-Column -Size 6 -Content { 
-                        New-Monitor -Title "Network (IO Read Bytes/sec)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#80E8611D' -ChartBorderColor '#FFE8611D'  -Endpoint {
+                New-UdRow {
+                    New-UdColumn -Size 6 -Content {
+                        New-UdMonitor -Title "Network (IO Read Bytes/sec)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#80E8611D' -ChartBorderColor '#FFE8611D'  -Endpoint {
 							try {
-								Get-Counter '\Process(_Total)\IO Read Bytes/sec' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-MonitorData
+								Get-Counter '\Process(_Total)\IO Read Bytes/sec' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
 							}
                             catch {
-								0 | Out-MonitorData
+								0 | Out-UDMonitorData
 							}
-                        } 
+                        }
                     }
-                    New-Column -Size 6 -Content { 
-                        New-Monitor -Title "Disk (% disk time)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#80E8611D' -ChartBorderColor '#FFE8611D'  -Endpoint {
+                    New-UdColumn -Size 6 -Content {
+                        New-UdMonitor -Title "Disk (% disk time)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#80E8611D' -ChartBorderColor '#FFE8611D'  -Endpoint {
 							try {
-								Get-Counter '\physicaldisk(_total)\% disk time' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-MonitorData
+								Get-Counter '\physicaldisk(_total)\% disk time' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
 							}
 							catch {
-								0 | Out-MonitorData
+								0 | Out-UDMonitorData
 							}
-                        } 
+                        }
                     }
                 }
-                New-Row {
-                    New-Column -Size 12 {
-                        New-Grid -Title "Processes" -Headers @("Name", "ID", "Working Set", "CPU") -Properties @("Name", "Id", "WorkingSet", "CPU") -AutoRefresh -RefreshInterval 60 -Endpoint {
-                            Get-Process | Out-GridData
+                New-UdRow {
+                    New-UdColumn -Size 12 {
+                        New-UdGrid -Title "Processes" -Headers @("Name", "ID", "Working Set", "CPU") -Properties @("Name", "Id", "WorkingSet", "CPU") -AutoRefresh -RefreshInterval 60 -Endpoint {
+                            Get-Process | Out-UDGridData
                         }
                     }
                 }
@@ -122,5 +122,3 @@ Start-Dashboard -Content {
         }
     }
 }
-
- 
