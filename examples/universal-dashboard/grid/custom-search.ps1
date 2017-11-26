bfda2bdb64169d7e0e0fc90cf7471a8c6f6b4525
Adam Driscoll <adam.r.driscoll@gmail.com>
2017-11-26 17:37:11 -0600

    Examples

---
+++ examples/universal-dashboard/grid/custom-search.ps1
@@ -0,0 +1,18 @@
+Import-Module UniversalDashboard
+
+$Data = @(
+    [PSCustomObject]@{Animal="Frog";Order="Anura"}
+    [PSCustomObject]@{Animal="Tiger";Order="Carnivora"}
+    [PSCustomObject]@{Animal="Bat";Order="Chiroptera"}
+    [PSCustomObject]@{Animal="Fox";Order="Carnivora"}
+)
+
+$Dashboard = New-UDDashboard -Title "Grids - Simple" -Content {
+    New-UDGrid -Title "Animals" -Headers @("Animal", "Order") -Properties @("Animal", "Order") -Endpoint {
+        #By default, filtering checks all properties. This overrides it to only check the animal property.
+        #$filterText is always provided in a grid endpoint
+        $Data | Where-Object Animal -Match $filterText | Out-UDGridData -NoAutoFilter
+    }
+}
+
+Start-UDDashboard -Dashboard $Dashboard -Port 8080
