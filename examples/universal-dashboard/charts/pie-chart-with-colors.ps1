bfe0593d0d60512c38da0ff1e43250a76351cf0a
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2018-02-21 15:25:50 -0600

    Update pie-chart-with-colors.ps1

--- examples/universal-dashboard/charts/pie-chart-with-colors.ps1
+++ examples/universal-dashboard/charts/pie-chart-with-colors.ps1
@@ -15,7 +15,7 @@ $Dashboard = New-UDDashboard -Title "Charts - Pie Colors" -Content {
 
     New-UDChart -Title "Pie Chart" -Type "Doughnut" -Endpoint {
         $Data | Out-UDChartData -LabelProperty "Animal"  -Dataset @(
-            New-UDLineChartDataset -Label "Animals" -DataProperty Count -BackgroundColor @("#2C44E8", "#FF4239", "#6FFF37","#FF912D") -BorderColor @("#000000", "#000000", "#000000","#000000") -BorderWidth 1
+            New-UDChartDataset -Label "Animals" -DataProperty Count -BackgroundColor @("#2C44E8", "#FF4239", "#6FFF37","#FF912D") -BorderColor @("#000000", "#000000", "#000000","#000000") -BorderWidth 1
         )
     } -Options @{cutoutPercentage = 0}
 }
